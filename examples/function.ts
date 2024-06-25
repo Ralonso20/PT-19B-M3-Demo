1a380d7958f06b3d1c5dc673842ef43c6f670823
Rodrigo Alonso <rodrigoalonso2000@gmail.com>
2024-06-24 22:52:52 -0300

    types ts

--- examples/function.ts
+++ examples/function.ts
@@ -5,3 +5,15 @@ function fullName(firstName: string, lastName: string): string | number {
     return 10;
   }
 }
+
+function multiples(a: number, b: number): boolean {
+  return a % b === 0;
+}
+
+function noReturn(): void {
+  console.log('I do not return anything');
+}
+
+fullName('John', 'Doe');
+console.log(multiples(10, 5));
+noReturn();
